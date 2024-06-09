6464d0593ce8e4017c9d0bd3715958741d03af67
Anjaleekrishna135 <anjaleekrishna135@gmail.com>
2024-06-09 14:40:15 +0530

    project created

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import AddBooks from './components/AddBooks';
 import SearchBook from './components/SearchBook';
 import DeleteBooks from './components/DeleteBooks';
 import ViewAll from './components/ViewAll';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddBooks />
-      <SearchBook />
-      <DeleteBooks />
-      <ViewAll />
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AddBooks />} />
+        <Route path='/search' element={<SearchBook />} />
+        <Route path='/delete' element={<DeleteBooks />} />
+        <Route path='/viewAll' element={<ViewAll />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <div>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="AddBooks.jsx">AddBooks</a>
+                    <Link class="navbar-brand" to="/">AddBooks</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="SearchBooks.jsx">Search</a>
+                                <Link class="nav-link active" aria-current="page" to="/search">Search</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="DeleteBooks.jsx">DeleteBooks</a>
+                                <Link class="nav-link" to="/delete">DeleteBooks</Link>
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" href="ViewAll.jsx">ViewAll</a>
+                                <Link class="nav-link" to="/ViewAll">ViewAll</Link>
                             </li>
                         </ul>
                         
